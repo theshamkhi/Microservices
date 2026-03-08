386c64c1cc5995827f6186c9b6fa813210cb56b3
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2026-03-08 13:23:07 +0000

    chore(service-a): add Spring Boot main entry point and enable Feign clients

---
+++ service-a/src/main/java/com/micro/servicea/ServiceAApplication.java
@@ -0,0 +1,13 @@
+package com.micro.servicea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ServiceAApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAApplication.class, args);
+    }
+}
